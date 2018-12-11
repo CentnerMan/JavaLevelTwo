94b9a075f7d90661a209d866203821ee10e8ab60
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2018-12-11 20:16:42 +0300

    Hello World

---
+++ src/main/java/ru/lebedev/se/App.java
@@ -0,0 +1,14 @@
+package ru.lebedev.se;
+
+/**
+ * @author Анатолий Лебедев
+ * @version 1.0.0
+ */
+
+public class App 
+{
+    public static void main(String[] args) {
+        System.out.println("Привет, мир!");
+    }
+
+}
