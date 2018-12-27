ded0f2d26c6bda08ec1cc6d28e6099e4146d9e5b
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2018-12-28 00:58:27 +0300

    Возвращает кол-во оставшихся рабочих часов до конца недели

--- src/main/java/ru/lebedev/se/lesson2dop/DayOfWeekMain.java
+++ src/main/java/ru/lebedev/se/lesson2dop/DayOfWeekMain.java
@@ -1,6 +1,7 @@
 package ru.lebedev.se.lesson2dop;
+
 /*
-Требуется реализовать enum DayOfWeek, который будет представлять дни недели.
+Требуется реализовать enum DayOfWeekMain, который будет представлять дни недели.
 С его помощью необходимо решить задачу определения кол-ва рабочих часов до конца недели по заднному текущему дню.
 
  Возвращает кол-во оставшихся рабочих часов до конца
@@ -11,8 +12,43 @@ package ru.lebedev.se.lesson2dop;
 public class DayOfWeekMain {
 
  public static void main(final String[] args) {
- System.out.println(getWorkingHours(DayOfWeek.MONDAY));
+ System.out.println(getWorkingHours(DayOfWeekMain.MONDAY));
  }
  */
-public class DayOfWeek {
+public class DayOfWeekMain {
+    enum DayOfWeek {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY}
+
+    public static void main(final String[] args) {
+        for (DayOfWeek s : DayOfWeek.values()) {
+            if (getWorkingHours(s) > 0)
+                System.out.println("Today is " + s.name() + ", before the weekend " + getWorkingHours(s) + " hours");
+            else System.out.println("The weekend has come. :)");
+        }
+    }
+
+    public static int rnd(int min, int max) { // случайное целое в диапазоне
+        max -= min;
+        return (int) (Math.random() * ++max) + min;
+    }
+
+    public static int getWorkingHours(DayOfWeek dayOfWeek) {
+        switch (dayOfWeek) {
+
+            case MONDAY:
+                return rnd(32, 40); // Рандомайзим количество оставшихся часов
+            case TUESDAY:
+                return rnd(24, 32);
+            case WEDNESDAY:
+                return rnd(16, 24);
+            case THURSDAY:
+                return rnd(8, 16);
+            case FRIDAY:
+                return rnd(0, 8);
+            case SATURDAY:
+                return 0;
+            case SUNDAY:
+                return 0;
+        }
+        return 0;
+    }
 }
