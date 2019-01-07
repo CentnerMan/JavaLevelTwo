28e3c1efbc6c45638e203d610591706ae98abb01
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2019-01-08 01:42:07 +0300

    validation

--- src/main/java/ru/lebedev/se/lesson3/validpass/PasswordValidator.java
+++ src/main/java/ru/lebedev/se/lesson3/validpass/PasswordValidator.java
@@ -1,4 +1,21 @@
 package ru.lebedev.se.lesson3.validpass;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PasswordValidator {
+    private static final String PASSWORD_PATTERN =
+            "(?=^[a-zA-Z0-9!@#$&%_-]{8,}$)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$&%])(?=.*\\d).*$";
+
+    private final Pattern pattern;
+    private Matcher matcher;
+
+    public PasswordValidator() {
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+    }
+
+    public boolean validate(String password) {
+        matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
 }
--- src/main/java/ru/lebedev/se/lesson3/validpass/Validation.java
+++ src/main/java/ru/lebedev/se/lesson3/validpass/Validation.java
@@ -1,4 +1,35 @@
 package ru.lebedev.se.lesson3.validpass;
 
+/*
+Необходимо из консоли считать пароль и проверить валидность,
+результат будет true или false
+
+Требования:
+1. Пароль должен быть не менее 8ми символов.
+2. В пароле должно быть число
+3. В пароле должна быть хотя бы 1 строчная буква
+4. В пароле должна быть хотя бы 1 заглавная буква
+5. Не должно быть пробелов
+6. Должен быть спец. символ
+---------------------------------------------------
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Validation {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String password;
+        System.out.println("Введите пароль. (Не менее 8 символов, a-z, без пробела, не менее 1 строчной, 1 прописной, 1 спецсимвол");
+        while (true) {
+            password = reader.readLine();
+            System.out.print("Не менее 8 символов, 1 строчная, 1 прописная, 1 спецсимвол из списка, 1 число: ");
+            System.out.println(password.matches("(?=^[a-zA-Z0-9!@#$&%_-]{8,}$)(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$&%])(?=.*\\d).*$"));
+        }
+    }
+
 }
