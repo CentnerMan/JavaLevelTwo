217e3f34525deabe475ecc538062ac0c709a23bf
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2019-01-14 01:11:26 +0300

    Параллельные вычисления для 2 потоков

--- src/main/java/ru/lebedev/se/lesson5/Multithreading/ParallelArray.java
+++ src/main/java/ru/lebedev/se/lesson5/Multithreading/ParallelArray.java
@@ -1,50 +1,83 @@
-/*
- * Copyright (c) 2019. Created by Anatoly Lebedev
- *
-1.	Необходимо написать два метода, которые делают следующее:
-1) Создают одномерный длинный массив, например:
-static final int size = 10 000 000;
-static final int h = size / 2;
-float[] arr = new float[size].
-2) Заполняют этот массив единицами.
-3) Засекают время выполнения: long a = System.currentTimeMillis().
-4) Проходят по всему массиву и для каждой ячейки считают новое значение по формуле:
-
-arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)).
-
-5) Проверяется время окончания метода System.currentTimeMillis().
-6) В консоль выводится время работы: System.out.println(System.currentTimeMillis() - a).
-
-Отличие первого метода от второго:
-●	Первый просто бежит по массиву и вычисляет значения.
-●	Второй разбивает массив на два массива, в двух потоках высчитывает новые значения и потом склеивает эти массивы обратно в один.
-
-Пример деления одного массива на два:
-●	System.arraycopy(arr, 0, a1, 0, h);
-●	System.arraycopy(arr, h, a2, 0, h).
-
-Пример обратной склейки:
-●	System.arraycopy(a1, 0, arr, 0, h);
-●	System.arraycopy(a2, 0, arr, h, h).
-
-Примечание:
-System.arraycopy() — копирует данные из одного массива в другой:
-System.arraycopy(массив-источник, откуда начинаем брать данные из массива-источника, массив-назначение, откуда начинаем записывать данные в массив-назначение, сколько ячеек копируем)
-По замерам времени:
-Для первого метода надо считать время только на цикл расчета:
-for (int i = 0; i < size; i++) {
-   arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
-}
-
-Для второго метода замеряете время разбивки массива на 2, просчета каждого из двух массивов и склейки.
-
- */
-
 package ru.lebedev.se.lesson5.Multithreading;
 
 /**
  * @author Anatoly Lebedev
  * @version 1.0
  */
+
 public class ParallelArray {
+    static final int size = 10000000;
+    static final int threads = 2; // количество потоков
+    static final int h = size / threads;
+    static float[] arr = new float[size];
+    static long timeToStart;
+    static long timeToEnd;
+
+    public static void initializeArray(float[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = 1f;
+        }
+    }
+
+    public static void calcAnything(float[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5)
+                    * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+    }
+
+    // Считаем одним потоком
+    static void SingleThreadArrayTimeMeasuring() {
+        // заполняем массив единицами
+        initializeArray(arr);
+        timeToStart = System.currentTimeMillis(); // засекаем начальное время
+
+        calcAnything(arr);
+
+        timeToEnd = System.currentTimeMillis(); // засекаем конечное время
+        System.out.println("Однопоточный режим: " + (timeToEnd - timeToStart)); // Выводим время выполнения процесса
+    }
+
+    static void MultipleThreadArrayTimeMeasuring() {
+        initializeArray(arr);
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+
+        timeToStart = System.currentTimeMillis(); // Засекаем время
+
+        // Делим на threads потоков
+
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+        // Считаем
+        Thread t1 = new Thread(() -> {
+            calcAnything(a1);
+        });
+        Thread t2 = new Thread(() -> {
+            calcAnything(a2);
+        });
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // Склеиваем
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+
+        timeToEnd = System.currentTimeMillis(); // засекаем конечное время
+        System.out.println("Многопоточный режим: " + (timeToEnd - timeToStart)); // Выводим время выполнения процесса
+    }
+
+
+    public static void main(String[] args) {
+        // Однопоточный режим
+        SingleThreadArrayTimeMeasuring();
+        // Многопоточный режим
+        MultipleThreadArrayTimeMeasuring();
+    }
 }
